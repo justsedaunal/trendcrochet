de6ee9f4063ec5123c09c3b8b1ab950397b589fd
Seda Ünal <sedaylmz06@outlook.com>
2022-03-08 01:11:31 +0300

    first commit

---
+++ crochettrend/lib/scripts.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        items:1,
+        loop:true,
+        margin:1,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoHeight: false,
+        autoplayHoverPause:true,
+        autoHeightClass: 'owl-height',
+        responsiveClass:true,
+        
+      
+    });
+  });
+
